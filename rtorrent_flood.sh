8201b37cc05e71e7c011c63a94048cc5e397343a
Dan Salmon <sa7mon@users.noreply.github.com>
2021-04-01 09:47:55 -0500

    Update rtorrent_flood.sh

--- rtorrent_flood.sh
+++ rtorrent_flood.sh
@@ -14,16 +14,16 @@ env ASSUME_ALWAYS_YES=yes pkg update -f
 yes "y" | pkg install tmux node npm rtorrent
 
 # Switch to our non-root user and pull down the config template that comes with rTorrent, enabling RPC socket file
-su $USERNAME
 curl -Ls "https://raw.githubusercontent.com/wiki/rakshasa/rtorrent/CONFIG-Template.md" \
     | sed -ne "/^######/,/^### END/p" \
     | sed -e 's/#network.scgi.open_local/network.scgi.open_local/' \
-    | sed -re "s:/home/USERNAME:$HOME:" > ~/.rtorrent.rc
-mkdir -p ~/rtorrent/
+    | sed -re "s:/home/USERNAME:$HOME:" > /home/$USERNAME/.rtorrent.rc
+mkdir -p /home/$USERNAME/rtorrent/
 
 # Pull down the startup shell script that will launch rtorrent and flood in a detached tmux session
-curl -Ls "https://raw.githubusercontent.com/sa7mon/iocages/master/rtorrent_flood_startup.sh" > ~/startup.sh
-chmod +x ~/startup.sh
+curl -Ls "https://raw.githubusercontent.com/sa7mon/iocages/master/rtorrent_flood_startup.sh" > /home/$USERNAME/startup.sh
+chmod +x /home/$USERNAME/startup.sh
 
 # Set startup script to run on boot
+su $USERNAME
 echo "@reboot /home/$USERNAME/startup.sh" | crontab -
